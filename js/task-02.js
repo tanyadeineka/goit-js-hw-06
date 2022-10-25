622f5d6b137e19d56c03c0f3c8c3f3439b29790c
Tetiana Deineka <tata4ka666@i.ua>
2022-10-25 18:47:09 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulList = document.getElementById("ingredients");
+
+const liArray = ingredients.map((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.classList.add("item");
+  liItem.textContent = ingredient;
+  return liItem;
+});
+ulList.append(...liArray);
