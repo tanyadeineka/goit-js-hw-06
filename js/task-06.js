dc277075283d795bdf8a377258b85c24444a7b5c
Tetiana Deineka <tata4ka666@i.ua>
2022-10-25 20:48:25 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.getElementById("validation-input");
+const lengthEl = inputEl.getAttribute("data-length");
+
+function inputElBlur() {
+  if (inputEl.value.length === Number(lengthEl)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+};
+
+inputEl.addEventListener("blur", inputElBlur);
