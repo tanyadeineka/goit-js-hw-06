94542287c5cf131735a817981719c7bd6d53ff88
Tetiana Deineka <tata4ka666@i.ua>
2022-10-26 16:43:58 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,5 @@
-let textInput = document.getElementById("name-input");
-let output = document.getElementById("name-output");
+let textInput = document.querySelector("#name-input");
+let output = document.querySelector("#name-output");
 
 textInput.oninput = function () {
   if (textInput.value === "") {
