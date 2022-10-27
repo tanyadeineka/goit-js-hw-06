5bd274781c4b552083edfa4c1eb7fc47c78e021a
Tetiana Deineka <tata4ka666@i.ua>
2022-10-27 16:36:03 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+}
