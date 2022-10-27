4c8fc645a3bebff6780acd5eee1b1df9bd6d8082
Tetiana Deineka <tata4ka666@i.ua>
2022-10-27 16:06:19 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -11,6 +11,7 @@ function handleSubmit(event) {
     if (email.value === "" || password.value === "") {
       return alert("Please fill in all the fields!");
     }
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    const userDetails = { Email: email.value, Password: password.value };
+    console.log(userDetails);
     event.currentTarget.reset();
 }
